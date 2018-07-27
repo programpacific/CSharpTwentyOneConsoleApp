acf114898427eddab1f999af2ac164ab258d73bb
programpacific <programpacific@users.noreply.github.com>
2018-07-27 15:28:23 -0600

    Relocated shuffle method to deck class.

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -9,9 +9,7 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-
-            int timesShuffled = 0;
-            deck = Shuffle(deck, out timesShuffled, 4);
+            deck.Shuffle();
 
             foreach (Card card in deck.Cards)
             {
