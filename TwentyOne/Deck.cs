2580a3e53ec071fbf34cec1f9d162f95955f3ddd
programpacific <programpacific@users.noreply.github.com>
2018-07-27 15:26:07 -0600

    Initial commit of source code.

---
+++ TwentyOne/Deck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>
+            { "Clubs", "Hearts", "Diamonds", "Spades" };
+
+            List<string> Faces = new List<string>()
+            { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+            };
+
+            foreach (string face in Faces)
+            {
+                foreach (string suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
+        }
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
+            {
+                timesShuffled++;
+                List<Card> tempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    tempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = tempList;
+            }
+        }
+    }
+}
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TwentyOne
 {
@@ -10,6 +8,29 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            Deck deck = new Deck();
+
+            int timesShuffled = 0;
+            deck = Shuffle(deck, out timesShuffled, 4);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times Shuffled: {0}", timesShuffled);
+            Console.ReadLine();
         }
+
+
+
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
     }
 }
