8a6b1d740eacb59126882bd39749f51c2733af46
programpacific <programpacific@users.noreply.github.com>
2018-08-04 11:43:07 -0600

    Added logging system to end of "Dealer" class.

--- TwentyOne/Dealer.cs
+++ TwentyOne/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -19,7 +20,12 @@ namespace TwentyOne
             // Writes what we are dealing.
             // Adding card to hand that is passed in from "Deck".
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine("\nxxxxxxxxxxxxxxxxxxxxxxxx\n" + Deck.Cards.First().ToString() + "\nxxxxxxxxxxxxxxxxxxxxxxxx\n"); // The "/n" adds a new line to the end.
+            string card = string.Format("\nxxxxxxxxxxxxxxxxxxxxxxxx\n" + Deck.Cards.First().ToString() + "\nxxxxxxxxxxxxxxxxxxxxxxxx\n");// The "/n" adds a new line to the end.
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\New\Desktop\TwentyOneLog.txt", true)) // The "using" keyword helps dispose of data at the end of operation to reduce memory consumption.
+            {
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
